e555401a830aead827be8fb095b69149881df5a0
Calalari <68549106+Calalari@users.noreply.github.com>
2023-03-10 01:25:46 +0300

    Add files via upload

---
+++ fdf.h
@@ -0,0 +1,11 @@
+
+#ifndef FDF_H
+#define FDF_H
+
+#include "../6_FdF/sources/minilibx/mlx.h"
+#include "../6_FdF/sources/minilibx/mlx_int.h"
+
+
+
+#endif
+
---
+++ main.c
@@ -0,0 +1,11 @@
+
+#include <mlx.h>
+#include <mlx_int.h>
+#include "fdf.h"
+
+int	main(void)
+{
+	void	*mlx;
+
+	mlx = mlx_init();
+}
