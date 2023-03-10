8c453c51453719ccc939cd8a3b328c881e0d5a10
Calalari <68549106+Calalari@users.noreply.github.com>
2023-03-10 19:03:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 16:08:44 by mervyilm          #+#    #+#             */
+/*   Updated: 2023/03/10 19:03:02 by mervyilm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "minilibx_macos/mlx.h"
+
+#include "fdf.h"
+
+int deal_key(int key, void *param)
+{
+	write(1, "X", 1);
+	return (0);
+}
 
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	t_fdf *data;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 400, 400, "mlx deneme");
-	mlx_loop(mlx_ptr);
+	data = malloc(sizeof(t_fdf));
+	if (data == 0x0)
+		return(1);
+	
+	data->mlx_ptr = mlx_init();
+	data->win_ptr = mlx_new_window(data->mlx_ptr, 400, 400, "MLX Window");
+	mlx_pixel_put(data->mlx_ptr, data->win_ptr, 200, 200, 0x90e0ef);
+	
+	//This function allows user to deal with a key
+	//Using this will allow writig on the terminal when pressed X
+	mlx_key_hook(data->win_ptr, deal_key, (void *)0);
+
+	mlx_loop(data->mlx_ptr);
 }
