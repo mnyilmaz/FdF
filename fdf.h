b7d52d4cbc7a46fb0b57492e4814f574dbc81c3c
Calalari <68549106+Calalari@users.noreply.github.com>
2023-03-10 19:04:08 +0300

    Update fdf.h

--- fdf.h
+++ fdf.h
@@ -1,11 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 16:08:42 by mervyilm          #+#    #+#             */
+/*   Updated: 2023/03/10 19:00:40 by mervyilm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #ifndef FDF_H
 #define FDF_H
 
-#include "../6_FdF/sources/minilibx/mlx.h"
-#include "../6_FdF/sources/minilibx/mlx_int.h"
+#include "minilibx_macos/mlx.h"
+#include "get_next_line/get_next_line.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
+typedef struct s_fdf
+{
+	int		height;
+	int		width;
+	int		**matrix;
+	void	*mlx_ptr;
+	void	*win_ptr;
 
+	struct s_fdf *next;
+}	t_fdf;
 
 #endif
-
