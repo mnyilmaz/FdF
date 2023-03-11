56242473ef7b12193becfced76f21ff40e605867
Calalari <68549106+Calalari@users.noreply.github.com>
2023-03-11 17:26:55 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 16:08:44 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/03/10 19:03:02 by mervyilm         ###   ########.fr       */
+/*   Updated: 2023/03/11 17:26:04 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,22 @@ int deal_key(int key, void *param)
 	return (0);
 }
 
+// int close_the_window(int button, void *param, t_fdf *ex)
+// {
+// 	write(1, "window closed", 13);
+// 	// mlx_clear_window(ex->mlx_ptr, ex->win_ptr);
+// 	// free(ex->win_ptr);
+// 	exit(1);
+// 	return (0);
+// ESC: 53
+// }
+
+int close_the_window(void)
+{
+	write (1, "asd\n", 4);
+	exit(0);
+	return (1);
+}
 int	main(void)
 {
 	t_fdf *data;
@@ -33,7 +49,8 @@ int	main(void)
 	
 	//This function allows user to deal with a key
 	//Using this will allow writig on the terminal when pressed X
-	mlx_key_hook(data->win_ptr, deal_key, (void *)0);
-
+	mlx_key_hook(data->win_ptr, deal_key, NULL);
+	 mlx_hook(data->win_ptr, 17, 0, close_the_window, (void *)0);
+	
 	mlx_loop(data->mlx_ptr);
 }
