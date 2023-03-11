90240c2eea42714001278d62cad1de662530d99f
Calalari <68549106+Calalari@users.noreply.github.com>
2023-03-11 17:28:27 +0300

    Update line_by_line.c

--- line_by_line.c
+++ line_by_line.c
@@ -6,22 +6,34 @@
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 16:08:38 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/03/10 18:29:22 by mervyilm         ###   ########.fr       */
+/*   Updated: 2023/03/11 12:08:29 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include <stdio.h>
 
 int	get_height(char	*file_name)
 {
 	int	fd;
 	int	height;
+	char	*str;
 	
-	fd = open()
-	
+	fd = open(file_name, O_RDONLY);
+	if (fd == -1)
+		return (1);
+	i = 0;
+	while(str = get_next_line(fd))
+	{
+		free(str);
+		height++;
+	}
+	close(fd);
+	printf("%d", height);
+	return(height);		
 }
 
 int	get_width(char	*file_name)
 {
-	
+	return(1);
 }
