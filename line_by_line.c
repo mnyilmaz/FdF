3a967a8c2342a1bd05ec21a9c13a3777b96caa57
Calalari <68549106+Calalari@users.noreply.github.com>
2023-03-17 15:33:23 +0300

    Update line_by_line.c

--- line_by_line.c
+++ line_by_line.c
@@ -6,15 +6,17 @@
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 16:08:38 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/03/11 12:08:29 by mervyilm         ###   ########.fr       */
+/*   Updated: 2023/03/17 15:30:07 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <stdio.h>
 
+
 int	get_height(char	*file_name)
 {
+	int	i;
 	int	fd;
 	int	height;
 	char	*str;
@@ -23,17 +25,49 @@ int	get_height(char	*file_name)
 	if (fd == -1)
 		return (1);
 	i = 0;
-	while(str = get_next_line(fd))
+	height = 0;
+	str = get_next_line(fd);
+	while(str)
 	{
 		free(str);
+		str = get_next_line(fd);
+
 		height++;
 	}
 	close(fd);
-	printf("%d", height);
-	return(height);		
+	printf("Height: %d\n", height);
+	return(height);
 }
 
 int	get_width(char	*file_name)
 {
-	return(1);
+	int	i;
+	int	fd;
+	int	height;
+	char	*str;
+	char	**box;
+	
+	fd = open(file_name, O_RDONLY);
+	if (fd == -1)
+		return (1);
+	
+	i = 0;
+	height = get_height(file_name);
+	while (height)
+	{
+		str = get_next_line(fd);
+		box = ft_split(str, ' ');
+		while(box[i])
+		{
+				printf("test\n");
+				box = ft_split(str, ' ');
+				printf("%d. indeks: %s\n", i, box[i]);
+				i++;
+				free_str(box);
+		}
+		free(str);
+		height--;
+	}
+	printf("Width: %d\n", i);
+	//free_str(box);
 }
