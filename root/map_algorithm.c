26d97a70c8a2eb9f4ca31a056d078a400b9cb256
Calalari <68549106+Calalari@users.noreply.github.com>
2023-03-29 02:16:55 +0300

    Update map_algorithm.c

--- root/map_algorithm.c
+++ root/map_algorithm.c
@@ -12,6 +12,14 @@
 
 #include "fdf.h"
 
+void	my_mlx_pixel_put(t_map *map, int x, int y, int colour)
+{
+	char	*dst;
+
+	dst = map->addr + (y * map->width + x * (map->bits / 8));
+	*(unsigned int*)dst = colour;
+}
+
 void	draw_line(t_map *map)
 {
 	while(map->x > 100)
@@ -29,10 +37,36 @@ void	draw_line(t_map *map)
 	}
 }
 
-void	my_mlx_pixel_put(t_map *map, int x, int y, int colour)
+void	draw_line_bresenham(float x, float y, float x1, float y1, t_map *map)
 {
-	char	*dst;
+	float	x_road;
+	float	y_road;
+	
+	x_road = x1 - x;
+	y_road = y1 - y;
+	while ((int)(x - x1)  || (int)(y - y1))
+	{
+		my_mlx_pixel_put(map, x, y, 0x90e0ef);
+		x += x_road;
+		y += y_road;
+	}
+}
 
-	dst = map->addr + (y * map->width + x * (map->bits / 8));
-	*(unsigned int*)dst = colour;
+void	actually_draw(t_map *map)
+{
+	int	x;
+	int	y;
+	
+	y = 0;
+	while (y < map->height)
+	{
+		x = 0;
+		while (x < map->width)
+		{
+			draw_line_bresenham(x, y, x+1, y, map);
+			draw_line_bresenham(x, y, x, y+1, map);
+			x++;
+		}
+		y++;
+	}
 }
